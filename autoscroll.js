6d48f59b9a3b9a163c50f3499994cbedeaa79b51
vmphuc1812 <vmphuc1812@gmail.com>
2016-03-01 23:15:30 +0700

    Added files via upload

---
+++ autoscroll.js
@@ -0,0 +1,132 @@
+<script type="text/javascript">
+	$(document).ready(function () {
+		// Dock each summary as it arrives just below the docked header, pushing the
+        // previous summary up the page.
+        var summaries = $('#fix_web_scroll');
+        summaries.each(function(i) {
+            var summary = $(summaries[i]);
+            var next = summaries[i + 1];
+            summary.scrollToFixed({
+                marginTop: $('.fix_web_scroll').outerHeight(true) + 10,
+                limit: function() {
+                    var limit = 0;
+                    if (next) {
+                        limit = $(next).offset().top - $(this).outerHeight(true) - 10;
+                    } else {
+                        limit = $('.panigation_news').offset().top - $(this).outerHeight(true) - 10;
+                    }
+                    return limit;
+                },
+                zIndex: 999
+            });
+        });
+	}); 
+</script>
+
+
+
+
+
+<script>
+$s='A@Ddac2@$&*@Sxfha';
+</script>	
+<script type="text/javascript">
+$(document).ready(function(){
+
+  if($s=='A@Ddac2@$&*@Sxfha'){ setTimeout(function() 
+  {
+    window.scroll(0,220);
+  }, 3400);
+}
+
+});
+</script>
+<script type="text/javascript">
+$(document).ready(function(){
+
+  if($s=='A@Ddac2@$&*@Sxfha'){ setTimeout(function() 
+  {
+    window.scroll(0,80);
+  }, 4500);
+}
+
+});
+</script>
+<script type="text/javascript">
+$(document).ready(function(){
+
+  if($s=='A@Ddac2@$&*@Sxfha'){ setTimeout(function() 
+  {
+    window.scroll(0,220);
+  }, 5700);
+}
+
+});
+</script>
+<script type="text/javascript">
+$(document).ready(function(){
+
+  if($s=='A@Ddac2@$&*@Sxfha'){ setTimeout(function() 
+  {
+    window.scroll(0,110);
+  }, 7200);
+}
+
+});
+</script>
+
+<script type="text/javascript">
+$(document).ready(function(){
+
+  if($s=='A@Ddac2@$&*@Sxfha'){ setTimeout(function() 
+  {
+    window.scroll(0,200);
+  }, 8200);
+}
+
+});
+</script>
+<script type="text/javascript">
+$(document).ready(function(){
+
+  if($s=='A@Ddac2@$&*@Sxfha'){ setTimeout(function() 
+  {
+    window.scroll(0,110);
+  }, 9200);
+}
+
+});
+</script>
+<script type="text/javascript">
+$(document).ready(function(){
+
+  if($s=='A@Ddac2@$&*@Sxfha'){ setTimeout(function() 
+  {
+    window.scroll(0,200);
+  }, 10000);
+}
+
+});
+</script>
+<script type="text/javascript">
+$(document).ready(function(){
+
+  if($s=='A@Ddac2@$&*@Sxfha'){ setTimeout(function() 
+  {
+    window.scroll(0,110);
+  }, 12000);
+}
+
+});
+</script>
+<script type="text/javascript">
+$(document).ready(function(){
+
+  if($s=='A@Ddac2@$&*@Sxfha'){ setTimeout(function() 
+  {
+    window.scroll(0,200);
+  }, 13000);
+}
+
+});
+</script>
